2b2ae5b899222b1ccf7a33aa4829e401e3e1f758
Mark Christoper Vizcarra <devforey@gmail.com>
2019-07-24 15:54:50 +0800

    Add edit page

--- src/app/contact/pages/contact-edit-page/contact-edit-page.component.ts
+++ src/app/contact/pages/contact-edit-page/contact-edit-page.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactDataSourceService } from '../../services/contact-data-source.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+import { Contact } from '../../types/contact.type';
 
 @Component({
   selector: 'app-contact-edit-page',
   templateUrl: './contact-edit-page.component.html',
-  styleUrls: ['./contact-edit-page.component.scss']
+  styleUrls: [ './contact-edit-page.component.scss' ]
 })
 export class ContactEditPageComponent implements OnInit {
+  public contact: Contact = {} as Contact;
 
-  constructor() { }
+  public constructor(
+    private contactDataSource: ContactDataSourceService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
-  ngOnInit() {
+  public ngOnInit() {
+    const params: Params = this.activatedRoute.snapshot.params;
+    if (!params || !params.id) {
+      this.router.navigateByUrl('contacts');
+      return;
+    }
+
+    this.contact = this.contactDataSource.getOne(params.id);
   }
 
+  public handleSubmit(contact: Contact) {
+    this.contactDataSource.edit(contact);
+    this.router.navigateByUrl('contacts');
+  }
 }
